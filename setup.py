8189a5ad47cdd9237765c615b83a3eaaad3624c1
Max MacCamy <maxmac12@gmail.com>
2018-04-16 16:52:14 -0700

    Cleanup

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-'''pyHook: Python wrapper for out-of-context input hooks in Windows
+"""
+pyHook: Python wrapper for out-of-context input hooks in Windows
 
 The pyHook package provides callbacks for global mouse and keyboard events in Windows. Python
 applications register event handlers for user input events such as left mouse down, left mouse up,
 key down, etc. and set the keyboard and/or mouse hook. The underlying C library reports information
 like the time of the event, the name of the window in which the event occurred, the value of the
 event, any keyboard modifiers, etc.
-'''
+"""
 
 classifiers = """\
 Development Status :: 5 - Production/Stable
@@ -30,11 +31,11 @@ setup(name='pyHook',
       download_url='http://www.sourceforge.net/projects/pyhook',
       license='http://www.opensource.org/licenses/mit-license.php',
       platforms=['Win32'],
-      description = doclines[0],
-      classifiers = filter(None, classifiers.split('\n')),
-      long_description = ' '.join(doclines[2:]),
-      packages = ['pyHook'],
-      package_dir = {'pyHook' : ""},
-      ext_modules = [Extension('pyHook._cpyHook', ['cpyHook.i'], libraries=libs)],
+      description=doclines[0],
+      classifiers=filter(None, classifiers.split('\n')),
+      long_description=' '.join(doclines[2:]),
+      packages=['pyHook'],
+      package_dir={'pyHook': ""},
+      ext_modules=[Extension('pyHook._cpyHook', ['cpyHook.i'], libraries=libs)],
       data_files=[('Lib/site-packages/pyHook', ['LICENSE.txt', 'README.md', 'CHANGELOG.txt'])]
       )
